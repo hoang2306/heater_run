40360f254d9360b3e2562251cb8d331956310111
Pham Duc Hoang <hoangggp@gmail.com>
2025-02-21 02:08:18 +0700

    'test'

--- model.py
+++ model.py
@@ -87,23 +87,140 @@ class Heater:
         self.eval_preds_warm = None  # the top-k predicted indices for warm evaluation
         self.eval_preds_cold = None  # the top-k predicted indices for cold evaluation
 
-    def build_model(self):
-        self.lr_placeholder = tf.placeholder(tf.float32, shape=[], name='learn_rate')
-        self.is_training = tf.placeholder(tf.bool, name='is_training')
-        self.target = tf.placeholder(tf.float32, shape=[None], name='target')
+    # def build_model(self):
+    #     self.lr_placeholder = tf.placeholder(tf.float32, shape=[], name='learn_rate')
+    #     self.is_training = tf.placeholder(tf.bool, name='is_training')
+    #     self.target = tf.placeholder(tf.float32, shape=[None], name='target')
 
-        self.Uin = tf.placeholder(tf.float32, shape=[None, self.rank_in], name='U_in_raw')
-        self.Vin = tf.placeholder(tf.float32, shape=[None, self.rank_in], name='V_in_raw')
+    #     self.Uin = tf.placeholder(tf.float32, shape=[None, self.rank_in], name='U_in_raw')
+    #     self.Vin = tf.placeholder(tf.float32, shape=[None, self.rank_in], name='V_in_raw')
+
+    #     dim = self.dim
+    #     self.reg_loss = 0.
+
+    #     if self.phi_v_dim > 0:
+    #         self.Vcontent = tf.placeholder(tf.float32, shape=[None, self.phi_v_dim], name='V_content')
+    #         self.dropout_item_indicator = tf.placeholder(tf.float32, shape=[None, 1], name='dropout_item_indicator')
+
+    #         vcontent_gate, vcontent_gate_reg = dense_fc(self.Vcontent, dim,
+    #                                                     'vcontent_gate_layer')  # size: batch_size X dim
+    #         vcontent_gate = tf.nn.tanh(vcontent_gate)
+
+    #         self.reg_loss += vcontent_gate_reg
+
+    #         vcontent_expert_list = []
+    #         for i in range(dim):
+    #             tmp_expert = self.Vcontent
+    #             for ihid, hid in enumerate(self.model_select):
+    #                 tmp_expert, tmp_reg = dense_fc(tmp_expert, hid, 'Vexpert_' + str(ihid) + '_' + str(i))
+    #                 tmp_expert = tf.nn.tanh(tmp_expert)
+    #                 self.reg_loss += tmp_reg
+    #             vcontent_expert_list.append(tf.reshape(tmp_expert, [-1, 1, self.rank_out]))
+
+    #         vcontent_expert_concat = tf.concat(vcontent_expert_list, 1)  # size: batch_size X dim X self.rank_out
+
+    #         vcontent_expert_concat = tf.linalg.matmul(tf.reshape(vcontent_gate, [-1, 1, dim]),
+    #                                                   vcontent_expert_concat)
+    #         Vcontent_last = tf.reshape(tf.nn.tanh(vcontent_expert_concat), [-1, self.rank_out])  # size: batch_size X self.rank_out
+
+    #         self.Vin_filter = 1 - self.dropout_item_indicator
+
+    #         diff_item_loss = self.alpha \
+    #                          * (tf.reduce_sum(tf.reduce_sum(tf.square(Vcontent_last - self.Vin),
+    #                                                         axis=1, keepdims=True)))
+    #         v_last = (self.Vin * self.Vin_filter + Vcontent_last * (1 - self.Vin_filter))
+    #     else:
+    #         v_last = self.Vin
+    #         diff_item_loss = 0
+
+    #     if self.phi_u_dim > 0:
+    #         self.Ucontent = tf.placeholder(tf.float32, shape=[None, self.phi_u_dim], name='U_content')
+    #         self.dropout_user_indicator = tf.placeholder(tf.float32, shape=[None, 1], name='dropout_user_indicator')
+
+    #         ucontent_gate, ucontent_gate_reg = dense_fc(self.Ucontent, dim,
+    #                                                     'ucontent_gate_layer')  # size: batch_size X dim
+    #         ucontent_gate = tf.nn.tanh(ucontent_gate)
+
+    #         self.reg_loss += ucontent_gate_reg
+
+    #         ucontent_expert_list = []
+    #         for i in range(dim):
+    #             tmp_expert = self.Ucontent
+    #             for ihid, hid in enumerate(self.model_select):
+    #                 tmp_expert, tmp_reg = dense_fc(tmp_expert, hid, 'Uexpert_' + str(ihid) + '_' + str(i))
+    #                 tmp_expert = tf.nn.tanh(tmp_expert)
+    #                 self.reg_loss += tmp_reg
+    #             ucontent_expert_list.append(tf.reshape(tmp_expert, [-1, 1, self.rank_out]))
+
+    #         ucontent_expert_concat = tf.concat(ucontent_expert_list, 1)  # size: batch_size X dim X self.rank_out
+
+    #         ucontent_expert_concat = tf.linalg.matmul(tf.reshape(ucontent_gate, [-1, 1, dim]),
+    #                                                   ucontent_expert_concat)
+    #         Ucontent_last = tf.reshape(tf.nn.tanh(ucontent_expert_concat), [-1, self.rank_out])  # size: batch_size X self.rank_out
+
+    #         self.Uin_filter = 1 - self.dropout_user_indicator
+
+    #         diff_user_loss = self.alpha \
+    #                          * (tf.reduce_sum(tf.reduce_sum(tf.square(Ucontent_last - self.Uin),
+    #                                                         axis=1, keepdims=True)))
+    #         u_last = (self.Uin * self.Uin_filter + Ucontent_last * (1 - self.Uin_filter))
+    #     else:
+    #         u_last = self.Uin
+    #         diff_user_loss = 0
+
+    #     for ihid, hid in enumerate([self.rank_out]):
+    #         u_last, u_reg = dense_batch_fc_tanh(u_last, hid, self.is_training, 'user_layer_%d'%ihid,
+    #                                             do_norm=True)
+    #         v_last, v_reg = dense_batch_fc_tanh(v_last, hid, self.is_training, 'item_layer_%d'%ihid,
+    #                                             do_norm=True)
+    #         self.reg_loss += u_reg
+    #         self.reg_loss += v_reg
+
+    #     with tf.variable_scope("U_embedding"):
+    #         u_emb_w = tf.Variable(tf.truncated_normal([u_last.get_shape().as_list()[1], self.rank_out], stddev=0.01),
+    #                               name='u_emb_w')
+    #         u_emb_b = tf.Variable(tf.zeros([1, self.rank_out]), name='u_emb_b')
+    #         self.U_embedding = tf.matmul(u_last, u_emb_w) + u_emb_b
+
+    #     with tf.variable_scope("V_embedding"):
+    #         v_emb_w = tf.Variable(tf.truncated_normal([v_last.get_shape().as_list()[1], self.rank_out], stddev=0.01),
+    #                               name='v_emb_w')
+    #         v_emb_b = tf.Variable(tf.zeros([1, self.rank_out]), name='v_emb_b')
+    #         self.V_embedding = tf.matmul(v_last, v_emb_w) + v_emb_b
+
+    #     self.reg_loss += (l2_norm(v_emb_w) + l2_norm(v_emb_b) + l2_norm(u_emb_w) + l2_norm(u_emb_b))
+    #     self.reg_loss *= self.reg
+
+    #     with tf.variable_scope("loss"):
+    #         preds = tf.multiply(self.U_embedding, self.V_embedding)
+    #         self.preds = tf.reduce_sum(preds, 1)  # output of the model, the predicted scores
+    #         self.diff_loss = diff_item_loss + diff_user_loss
+    #         self.loss = tf.reduce_mean(tf.squared_difference(self.preds, self.target)) + self.reg_loss + self.diff_loss
+
+    #     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    #     with tf.control_dependencies(update_ops):
+    #         # Ensures that we execute the update_ops before performing the train_step
+    #         self.optimizer = tf.train.MomentumOptimizer(self.lr_placeholder, 0.9).minimize(self.loss)
+
+    def build_model(self):
+        # Sử dụng tf.Variable cho lr_placeholder
+        self.lr_placeholder = tf.Variable(0.001, dtype=tf.float32, name='learn_rate')
+
+        # Sử dụng tf.keras.Input cho các giá trị kiểu placeholder
+        self.is_training = tf.keras.Input(shape=(), dtype=tf.bool, name='is_training')
+        self.target = tf.keras.Input(shape=[None], dtype=tf.float32, name='target')
+
+        self.Uin = tf.keras.Input(shape=[None, self.rank_in], dtype=tf.float32, name='U_in_raw')
+        self.Vin = tf.keras.Input(shape=[None, self.rank_in], dtype=tf.float32, name='V_in_raw')
 
         dim = self.dim
         self.reg_loss = 0.
 
         if self.phi_v_dim > 0:
-            self.Vcontent = tf.placeholder(tf.float32, shape=[None, self.phi_v_dim], name='V_content')
-            self.dropout_item_indicator = tf.placeholder(tf.float32, shape=[None, 1], name='dropout_item_indicator')
+            self.Vcontent = tf.keras.Input(shape=[None, self.phi_v_dim], dtype=tf.float32, name='V_content')
+            self.dropout_item_indicator = tf.keras.Input(shape=[None, 1], dtype=tf.float32, name='dropout_item_indicator')
 
-            vcontent_gate, vcontent_gate_reg = dense_fc(self.Vcontent, dim,
-                                                        'vcontent_gate_layer')  # size: batch_size X dim
+            vcontent_gate, vcontent_gate_reg = dense_fc(self.Vcontent, dim, 'vcontent_gate_layer')
             vcontent_gate = tf.nn.tanh(vcontent_gate)
 
             self.reg_loss += vcontent_gate_reg
@@ -117,28 +234,25 @@ class Heater:
                     self.reg_loss += tmp_reg
                 vcontent_expert_list.append(tf.reshape(tmp_expert, [-1, 1, self.rank_out]))
 
-            vcontent_expert_concat = tf.concat(vcontent_expert_list, 1)  # size: batch_size X dim X self.rank_out
+            vcontent_expert_concat = tf.concat(vcontent_expert_list, 1)
 
-            vcontent_expert_concat = tf.linalg.matmul(tf.reshape(vcontent_gate, [-1, 1, dim]),
-                                                      vcontent_expert_concat)
-            Vcontent_last = tf.reshape(tf.nn.tanh(vcontent_expert_concat), [-1, self.rank_out])  # size: batch_size X self.rank_out
+            vcontent_expert_concat = tf.linalg.matmul(tf.reshape(vcontent_gate, [-1, 1, dim]), vcontent_expert_concat)
+            Vcontent_last = tf.reshape(tf.nn.tanh(vcontent_expert_concat), [-1, self.rank_out])
 
             self.Vin_filter = 1 - self.dropout_item_indicator
 
-            diff_item_loss = self.alpha \
-                             * (tf.reduce_sum(tf.reduce_sum(tf.square(Vcontent_last - self.Vin),
-                                                            axis=1, keepdims=True)))
+            diff_item_loss = self.alpha * (tf.reduce_sum(tf.reduce_sum(tf.square(Vcontent_last - self.Vin),
+                                                                    axis=1, keepdims=True)))
             v_last = (self.Vin * self.Vin_filter + Vcontent_last * (1 - self.Vin_filter))
         else:
             v_last = self.Vin
             diff_item_loss = 0
 
         if self.phi_u_dim > 0:
-            self.Ucontent = tf.placeholder(tf.float32, shape=[None, self.phi_u_dim], name='U_content')
-            self.dropout_user_indicator = tf.placeholder(tf.float32, shape=[None, 1], name='dropout_user_indicator')
+            self.Ucontent = tf.keras.Input(shape=[None, self.phi_u_dim], dtype=tf.float32, name='U_content')
+            self.dropout_user_indicator = tf.keras.Input(shape=[None, 1], dtype=tf.float32, name='dropout_user_indicator')
 
-            ucontent_gate, ucontent_gate_reg = dense_fc(self.Ucontent, dim,
-                                                        'ucontent_gate_layer')  # size: batch_size X dim
+            ucontent_gate, ucontent_gate_reg = dense_fc(self.Ucontent, dim, 'ucontent_gate_layer')
             ucontent_gate = tf.nn.tanh(ucontent_gate)
 
             self.reg_loss += ucontent_gate_reg
@@ -152,17 +266,15 @@ class Heater:
                     self.reg_loss += tmp_reg
                 ucontent_expert_list.append(tf.reshape(tmp_expert, [-1, 1, self.rank_out]))
 
-            ucontent_expert_concat = tf.concat(ucontent_expert_list, 1)  # size: batch_size X dim X self.rank_out
+            ucontent_expert_concat = tf.concat(ucontent_expert_list, 1)
 
-            ucontent_expert_concat = tf.linalg.matmul(tf.reshape(ucontent_gate, [-1, 1, dim]),
-                                                      ucontent_expert_concat)
-            Ucontent_last = tf.reshape(tf.nn.tanh(ucontent_expert_concat), [-1, self.rank_out])  # size: batch_size X self.rank_out
+            ucontent_expert_concat = tf.linalg.matmul(tf.reshape(ucontent_gate, [-1, 1, dim]), ucontent_expert_concat)
+            Ucontent_last = tf.reshape(tf.nn.tanh(ucontent_expert_concat), [-1, self.rank_out])
 
             self.Uin_filter = 1 - self.dropout_user_indicator
 
-            diff_user_loss = self.alpha \
-                             * (tf.reduce_sum(tf.reduce_sum(tf.square(Ucontent_last - self.Uin),
-                                                            axis=1, keepdims=True)))
+            diff_user_loss = self.alpha * (tf.reduce_sum(tf.reduce_sum(tf.square(Ucontent_last - self.Uin),
+                                                                    axis=1, keepdims=True)))
             u_last = (self.Uin * self.Uin_filter + Ucontent_last * (1 - self.Uin_filter))
         else:
             u_last = self.Uin
@@ -178,13 +290,13 @@ class Heater:
 
         with tf.variable_scope("U_embedding"):
             u_emb_w = tf.Variable(tf.truncated_normal([u_last.get_shape().as_list()[1], self.rank_out], stddev=0.01),
-                                  name='u_emb_w')
+                                name='u_emb_w')
             u_emb_b = tf.Variable(tf.zeros([1, self.rank_out]), name='u_emb_b')
             self.U_embedding = tf.matmul(u_last, u_emb_w) + u_emb_b
 
         with tf.variable_scope("V_embedding"):
             v_emb_w = tf.Variable(tf.truncated_normal([v_last.get_shape().as_list()[1], self.rank_out], stddev=0.01),
-                                  name='v_emb_w')
+                                name='v_emb_w')
             v_emb_b = tf.Variable(tf.zeros([1, self.rank_out]), name='v_emb_b')
             self.V_embedding = tf.matmul(v_last, v_emb_w) + v_emb_b
 
@@ -199,7 +311,6 @@ class Heater:
 
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         with tf.control_dependencies(update_ops):
-            # Ensures that we execute the update_ops before performing the train_step
             self.optimizer = tf.train.MomentumOptimizer(self.lr_placeholder, 0.9).minimize(self.loss)
 
     def build_predictor(self, recall_at):
